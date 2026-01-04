17b8fd4ec7146f1f9750a7d2c804865814e8ce28
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-01-04 13:00:54 +0900

    Admin login

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class UserDTO
+    {
+        public int ID {  get; set; }
+        [Required(ErrorMessage = "Please fill the Username Area")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please fill the Password Area")]
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Imagepath { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
